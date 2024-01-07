e64a28dc3f4abbc99dd204541eca22c33f48531a
PRoy <sam469roger@gmail.com>
2024-01-07 12:41:31 +0530

    feat-main: lifting state up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 3, description: 'Charger', quantity: 1, packed: true },
-];
-
+//! App component (parent)
 function App() {
+  //* lifted state from child Form component to parent App
+  //* now state can be passed to all child cpmponents
+  //* ie siblings of Form component from where this state was lifted
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
 }
 
+//! Logo component (child to app & sibling)
 function Logo() {
   return <h1>🌴 Far Away 🎒</h1>;
 }
 
-function Form() {
+//! Form component (child to app & sibling)
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -37,7 +47,9 @@ function Form() {
       packed: false,
     };
 
-    console.log(newItem);
+    //* function to update items piece of state which was lifted to parent component
+    //* state updated when form is submitted
+    onAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -67,29 +79,32 @@ function Form() {
   );
 }
 
-function PackingList() {
+//! PackingList component (child to app & sibling)
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item key={item.id} {...item} />
+        {items.map((item) => (
+          <Item key={item.id} {...item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ description, quantity, packed }) {
+//! Item component (child to Packinglist)
+function Item({ id, description, quantity, packed, onDeleteItem }) {
   return (
     <li>
       <span style={packed ? { textDecoration: 'line-through' } : {}}>
         {quantity} {description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(id)}>❌</button>
     </li>
   );
 }
 
+//! Stats component (child to app & sibling)
 function Stats() {
   return (
     <footer className="stats">
